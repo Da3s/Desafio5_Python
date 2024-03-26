e000f64bc2bfd8be96348064144a5ae95732207b
Daniel <danielmorernoinf@gmail.com>
2024-03-26 19:56:47 -0300

    se modifica operaciones

--- ong.py
+++ ong.py
@@ -24,4 +24,4 @@ def calcular(**parametros):
         else:
             print(f'La productoria de {valor} es {productoria(valor)}')
             
-calcular(fact_1 = 5, prod_1 = [3,6,4,2,8], fact_2 = 6)
+calcular(fact_1 = 5, prod_1 =  [4, 6, 7, 4, 3], fact_2 = 6)
